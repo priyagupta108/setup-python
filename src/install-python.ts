46d885c777d49f344e31eb3e0de9e7ce73f2213b
Priyagupta108 <priyagupta108@github.com>
2024-11-04 15:12:43 +0530

    create_release_test

--- src/install-python.ts
+++ src/install-python.ts
@@ -8,9 +8,9 @@ import {IS_WINDOWS, IS_LINUX, getDownloadFileName} from './utils';
 
 const TOKEN = core.getInput('token');
 const AUTH = !TOKEN ? undefined : `token ${TOKEN}`;
-const MANIFEST_REPO_OWNER = 'actions';
+const MANIFEST_REPO_OWNER = 'priyagupta108';
 const MANIFEST_REPO_NAME = 'python-versions';
-const MANIFEST_REPO_BRANCH = 'main';
+const MANIFEST_REPO_BRANCH = 'update-versions-manifest-file';
 export const MANIFEST_URL = `https://raw.githubusercontent.com/${MANIFEST_REPO_OWNER}/${MANIFEST_REPO_NAME}/${MANIFEST_REPO_BRANCH}/versions-manifest.json`;
 
 export async function findReleaseFromManifest(
